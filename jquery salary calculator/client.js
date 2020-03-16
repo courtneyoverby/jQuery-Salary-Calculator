3a749ee8b68f3e20a13569028b2688b4c8ce764f
Courtney Overby <brioverby@gmail.com>
2020-03-15 20:06:42 -0500

    pushing into array

--- jquery salary calculator/client.js
+++ jquery salary calculator/client.js
@@ -1,1 +1,25 @@
 console.log("Hello!");
+
+const information = [];
+
+$(document).ready(init);
+
+function init() {
+  $(".js-employee-stats").on("submit", submitEmployeeInfo);
+}
+
+function submitEmployeeInfo(event) {
+  event.preventDefault();
+  console.log("Hellooooo");
+
+  const employeeInfo = {
+    firstName: $(`.js-employee-FN`).val(),
+    lastName: $(`.js-employee-LN`).val(),
+    ID: $(`.js-employee-ID`).val(),
+    jobTitle: $(`.js-employee-title`).val(),
+    annSalary: $(`.js-employee-salary`).val()
+  };
+
+  information.push(employeeInfo);
+  console.log(information);
+}
