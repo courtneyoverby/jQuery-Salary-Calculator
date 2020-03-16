3a4c3a2c6d85c79416c2a989d3ef59acd69f0bbd
Courtney Overby <brioverby@gmail.com>
2020-03-15 21:57:52 -0500

    deleting items from arrray

--- jquery salary calculator/client.js
+++ jquery salary calculator/client.js
@@ -6,6 +6,18 @@ $(document).ready(init);
 
 function init() {
   $(".js-employee-stats").on("submit", submitEmployeeInfo);
+  $(".js-table-body").on("click", ".js-btn-delete", deleteEmployeeInfo);
+}
+
+function deleteEmployeeInfo() {
+  console.log(this);
+  $(this)
+    .parent()
+    .parent()
+    .remove();
+  const infoIndex = $(this).data("index");
+
+  information.splice(infoIndex, 1);
 }
 
 function submitEmployeeInfo(event) {
@@ -17,9 +29,32 @@ function submitEmployeeInfo(event) {
     lastName: $(`.js-employee-LN`).val(),
     ID: $(`.js-employee-ID`).val(),
     jobTitle: $(`.js-employee-title`).val(),
-    annSalary: $(`.js-employee-salary`).val()
+    annSalary: parseFloat($(`.js-employee-salary`).val())
   };
 
   information.push(employeeInfo);
   console.log(information);
+  render();
+}
+
+function render() {
+  $(".js-table-body").empty();
+  let totalSalary = 0;
+  for (let i = 0; i < information.length; i++) {
+    const employeeinformation = information[i];
+    totalSalary = totalSalary + employeeinformation.annSalary;
+
+    $(".js-table-body").append(`
+        <tr>
+        <td>${employeeinformation.firstName}</td>
+        <td>${employeeinformation.lastName}</td>
+        <td>${employeeinformation.ID}</td>
+        <td>${employeeinformation.jobTitle}</td>
+        <td>$${employeeinformation.annSalary}</td>
+        <td><button class="js-btn-delete" data-index='${i}'>Delete</button></td>
+        </tr>
+        `);
+  }
+
+  $(".js-total-salary").text(totalSalary);
 }
